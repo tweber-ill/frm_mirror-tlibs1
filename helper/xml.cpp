a5189570ae68caa4fce982e0c15a23f58b7e3402
Tobias Weber <tobias.weber@frm2.tum.de>
2013-04-23 15:12:54 +0200

    added xml helper class

--- helper/xml.cpp
+++ helper/xml.cpp
@@ -5,25 +5,61 @@
  */
 
 #include "xml.h"
+#include "../helper/string.h"
+
+#include <QtXmlPatterns/QXmlQuery>
+#include <QtCore/QString>
+#include <QtCore/QStringList>
+#include <QtCore/QFile>
+#include <QtCore/QTextStream>
+
 
 Xml::Xml()
 {}
-
 
 Xml::~Xml()
 { }
 
 bool Xml::Load(const char* pcFile)
 {
-	return false;
+	QFile file(pcFile);
+	if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+		return false;
+
+	QTextStream istr(&file);
+	QString strXml = istr.readAll();
+
+	m_bufXml.close();
+	m_bufXml.setData(strXml.toUtf8());
+	m_bufXml.open(QIODevice::ReadOnly);
+
+	file.close();
+	return true;
 }
 
-std::string Xml::QueryString(const char* pcAdr, const char* pcDef, bool *pbOk) const
+std::string Xml::QueryString(const char* pcAdr, const char* pcDef, bool *pbOk)
 {
-	return "";
-}
+	std::ostringstream ostrQuery;
+	ostrQuery << "doc($xml)" << pcAdr << "/string()";
 
-double Xml::QueryDouble(const char* pcAdr, double dDef, bool *pbOk) const
-{
-	return 0.;
+	QXmlQuery query;
+	query.bindVariable("xml", &m_bufXml);
+	query.setQuery(QString(ostrQuery.str().c_str()));
+
+	QStringList strlstOut;
+	bool bOk = query.evaluateTo(&strlstOut);
+	if(!bOk || strlstOut.size()==0)
+	{
+		if(pbOk) *pbOk = false;
+
+		if(pcDef)
+			return std::string(pcDef);
+		return "";
+	}
+
+	if(pbOk) *pbOk = true;
+	std::string strOut = strlstOut.at(0).toStdString();
+	::trim(strOut);
+
+	return strOut;
 }
--- helper/xml.h
+++ helper/xml.h
@@ -8,10 +8,14 @@
 #define __MIEZE_XML__
 
 #include <string>
+#include <sstream>
+
+#include <QtCore/QBuffer>
 
 class Xml
 {
 protected:
+	QBuffer m_bufXml;
 
 public:
 	Xml();
@@ -19,8 +23,30 @@ public:
 
 	bool Load(const char* pcFile);
 
-	std::string QueryString(const char* pcAdr, const char* pcDef, bool *pbOk) const;
-	double QueryDouble(const char* pcAdr, double dDef, bool *pbOk) const;
+	std::string QueryString(const char* pcAdr, const char* pcDef, bool *pbOk);
+
+	template<typename T>
+	T Query(const char* pcAdr, T tDef, bool *pbOk)
+	{
+		std::ostringstream ostrDef;
+		ostrDef << tDef;
+
+		bool bOk = 0;
+		std::string str = QueryString(pcAdr, ostrDef.str().c_str(), &bOk);
+		if(pbOk) *pbOk = bOk;
+
+		if(bOk)
+		{
+			T tRes(0);
+
+			std::istringstream istr(str);
+			istr >> tRes;
+
+			return tRes;
+		}
+		else
+			return tDef;
+	}
 };
 
 #endif
