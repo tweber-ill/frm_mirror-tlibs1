b360e3d595ac8d90d86bbb53c0b95f48666dcc65
Tobias Weber <tobias.weber@frm2.tum.de>
2013-07-17 07:47:32 +0200

    a) finished combine dialog; b) better handling of mdi windows

--- helper/string.h
+++ helper/string.h
@@ -16,76 +16,14 @@ extern std::string
 insert_before(const std::string& str,
 				const std::string& strChar,
 				const std::string& strInsert);
+extern std::string get_fileext(const std::string& str);
+extern std::string get_dir(const std::string& str);
+extern std::string get_file(const std::string& str);
+extern bool is_equal(const std::string& str0, const std::string& str1, bool bCase=false);
+extern void trim(std::string& str);
+extern void find_and_replace(std::string& str1, const std::string& str_old,
+                                                const std::string& str_new);
 
-inline std::string get_fileext(const std::string& str)
-{
-	std::size_t iPos = str.find_last_of('.');
-
-	if(iPos == std::string::npos)
-		return std::string("");
-	return str.substr(iPos+1);
-}
-
-inline std::string get_dir(const std::string& str)
-{
-	std::size_t iPos = str.find_last_of("\\/");
-
-	if(iPos == std::string::npos)
-		return std::string("");
-	return str.substr(0, iPos);
-}
-
-inline std::string get_file(const std::string& str)
-{
-	std::size_t iPos = str.find_last_of("\\/");
-
-	if(iPos == std::string::npos)
-		return std::string("");
-	return str.substr(iPos+1);
-}
-
-inline bool is_equal(const std::string& str0, const std::string& str1, bool bCase=false)
-{
-	if(bCase) return str0==str1;
-
-	if(str0.size() != str1.size())
-		return false;
-
-	for(unsigned int i=0; i<str0.size(); ++i)
-	{
-		if(tolower(str0[i]) != tolower(str1[i]))
-			return false;
-	}
-	return true;
-}
-
-inline void trim(std::string& str)
-{
-        std::size_t posFirst = str.find_first_not_of(" \t");
-        if(posFirst==std::string::npos)
-        	posFirst = str.length();
-
-        str.erase(str.begin(), str.begin()+posFirst);
-
-
-        std::size_t posLast = str.find_last_not_of(" \t");
-        if(posLast==std::string::npos)
-                posLast = str.length();
-        else
-        	++posLast;
-
-        str.erase(str.begin()+posLast, str.end());
-}
-
-inline void find_and_replace(std::string& str1, const std::string& str_old,
-                                                const std::string& str_new)
-{
-        std::size_t pos = str1.find(str_old);
-        if(pos==std::string::npos)
-                return;
-
-        str1.replace(pos, str_old.length(), str_new);
-}
 
 template<class T>
         void get_tokens(const std::string& str, const std::string& strDelim,
