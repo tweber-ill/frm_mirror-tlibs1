29a9452ef657c1379e155c97e9e0c0c774f0f8b9
Tobias Weber <tobias.weber@frm2.tum.de>
2013-08-28 08:59:55 +0200

    continued with compression

--- helper/comp.cpp
+++ helper/comp.cpp
@@ -10,16 +10,108 @@
 #include <fstream>
 #include <list>
 
+#include <boost/iostreams/stream.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
 #include <boost/iostreams/filter/bzip2.hpp>
 #include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/device/array.hpp>
 #include <boost/iostreams/copy.hpp>
 
 namespace ios = boost::iostreams;
 
-bool decomp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor comp)
+
+static Compressor comp_from_magic(const unsigned char* pcMagic, unsigned int iLen)
+{
+	if(iLen<2)
+		return COMP_INVALID;
+
+	if(pcMagic[0]==0x1f && pcMagic[1]==0x8b)
+		return COMP_GZ;
+
+	if(pcMagic[0]==0x78 && (pcMagic[1]==0xda || pcMagic[1]==0x9c || pcMagic[1]==0x01))
+		return COMP_Z;
+
+
+	if(iLen<3)
+		return COMP_INVALID;
+
+	if(pcMagic[0]=='B' && pcMagic[1]=='Z' && pcMagic[2]=='h')
+		return COMP_BZ2;
+
+
+	return COMP_INVALID;
+}
+
+
+//--------------------------------------------------------------------------------
+
+
+bool decomp_stream_to_stream(std::istream& istr, std::ostream& ostr, Compressor comp)
+{
+	if(comp == COMP_AUTO)
+	{
+		unsigned char pcMagic[3] = {0,0,0};
+		istr.read((char*)pcMagic, 3);
+		istr.seekg(0,std::ios::beg);
+
+		comp = comp_from_magic(pcMagic, 3);
+	}
+
+
+	ios::filtering_streambuf<ios::input> input;
+
+	if(comp == COMP_GZ)
+		input.push(ios::gzip_decompressor());
+	else if(comp == COMP_BZ2)
+		input.push(ios::bzip2_decompressor());
+	else if(comp == COMP_Z)
+		input.push(ios::zlib_decompressor());
+	else
+	{
+		std::cerr << "Error: Unknown decompression selected." << std::endl;
+		return false;
+	}
+
+	input.push(istr);
+	ios::copy(input, ostr);
+
+	return true;
+}
+
+bool comp_stream_to_stream(std::istream& istr, std::ostream& ostr, Compressor comp)
+{
+	if(comp == COMP_AUTO)
+		comp = COMP_GZ;
+
+
+	ios::filtering_streambuf<ios::input> input;
+
+	if(comp == COMP_GZ)
+		input.push(ios::gzip_compressor());
+	else if(comp == COMP_BZ2)
+		input.push(ios::bzip2_compressor());
+	else if(comp == COMP_Z)
+		input.push(ios::zlib_compressor());
+	else
+	{
+		std::cerr << "Error: Unknown compression selected." << std::endl;
+		return false;
+	}
+
+	input.push(istr);
+	ios::copy(input, ostr);
+
+	return true;
+}
+
+
+//--------------------------------------------------------------------------------
+
+
+static inline bool __comp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor comp, bool bDecomp=0)
 {
 	std::ifstream ifstr(pcFileIn, std::ios_base::binary);
 	if(!ifstr.is_open())
@@ -37,59 +129,54 @@ bool decomp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor
 	}
 
 
-	ios::filtering_streambuf<ios::input> input;
-
 	if(comp == COMP_AUTO)
 	{
-		std::string strExt = get_fileext(pcFileIn);
+		std::string strExt;
+		if(bDecomp)
+			strExt = get_fileext(pcFileIn);
+		else
+			strExt = get_fileext(pcFileOut);
+
 		if(strExt == "gz")
 			comp = COMP_GZ;
 		else if(strExt == "bz2")
 			comp = COMP_BZ2;
 	}
 
-	if(comp == COMP_GZ)
-		input.push(ios::gzip_decompressor());
-	else if(comp == COMP_BZ2)
-		input.push(ios::bzip2_decompressor());
+	if(bDecomp)
+		return decomp_stream_to_stream(ifstr, ofstr, comp);
 	else
-	{
-		std::cerr << "Error: Unknown decompression for \"" << pcFileIn << "\"." << std::endl;
-		return false;
-	}
+		return comp_stream_to_stream(ifstr, ofstr, comp);
+}
 
-	input.push(ifstr);
+bool comp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor comp)
+{
+	return __comp_file_to_file(pcFileIn, pcFileOut, comp, 0);
+}
 
-	ios::copy(input, ofstr);
-
-	return true;
+bool decomp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor comp)
+{
+	return __comp_file_to_file(pcFileIn, pcFileOut, comp, 1);
 }
 
 
-bool decomp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp)
+//--------------------------------------------------------------------------------
+
+
+static inline bool __comp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp, bool bDecomp=0)
 {
 	char *pcIn = (char*)pvIn;
-	ios::basic_array_source<char> arrIn(pcIn, pcIn+iLenIn);
+	ios::stream<ios::basic_array_source<char> > istr(pcIn, iLenIn);
 
 	std::list<char> lstOut;
 	ios::filtering_ostream arrOut(ios::back_inserter(lstOut));
 
 
-	ios::filtering_streambuf<ios::input> input;
-
-	if(comp == COMP_GZ)
-		input.push(ios::gzip_decompressor());
-	else if(comp == COMP_BZ2)
-		input.push(ios::bzip2_decompressor());
+	bool bOk=0;
+	if(bDecomp)
+		bOk = decomp_stream_to_stream(istr, arrOut, comp);
 	else
-	{
-		std::cerr << "Error: Unknown decompression selected." << std::endl;
-		return false;
-	}
-
-	input.push(arrIn);
-
-	ios::copy(input, arrOut);
+		bOk =comp_stream_to_stream(istr, arrOut, comp);
 
 
 	iLenOut = lstOut.size();
@@ -100,5 +187,26 @@ bool decomp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned i
 	for(char c : lstOut)
 		pcOut[iIdx++] = c;
 
-	return true;
+	return bOk;
+}
+
+/*
+ * example:
+ *	char pc[] = "123456\nABCDEF\n\n";
+ *	void *pvTst;
+ *	unsigned int iLenOut=0;
+ *	::comp_mem_to_mem(pc, strlen(pc), pvTst,iLenOut,COMP_BZ2);
+ *	std::ofstream ofstr("tst.txt.bz2");
+ *	ofstr.write((char*)pvTst, iLenOut);
+ *	ofstr.close();
+ *	delete[] pvTst;
+ */
+bool comp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp)
+{
+	return __comp_mem_to_mem(pvIn, iLenIn, pvOut, iLenOut, comp, 0);
+}
+
+bool decomp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp)
+{
+	return __comp_mem_to_mem(pvIn, iLenIn, pvOut, iLenOut, comp, 1);
 }
--- helper/comp.h
+++ helper/comp.h
@@ -7,15 +7,24 @@
 #ifndef __M_COMP_H__
 #define __M_COMP_H__
 
+#include <iostream>
+
 enum Compressor
 {
 	COMP_GZ,
 	COMP_BZ2,
+	COMP_Z,
 
-	COMP_AUTO
+	COMP_AUTO,
+	COMP_INVALID
 };
 
+extern bool decomp_stream_to_stream(std::istream& istr, std::ostream& ostr, Compressor comp=COMP_AUTO);
 extern bool decomp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor comp=COMP_AUTO);
-extern bool decomp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp=COMP_GZ);
+extern bool decomp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp=COMP_AUTO);
+
+extern bool comp_stream_to_stream(std::istream& istr, std::ostream& ostr, Compressor comp=COMP_AUTO);
+extern bool comp_file_to_file(const char* pcFileIn, const char* pcFileOut, Compressor comp=COMP_GZ);
+extern bool comp_mem_to_mem(void* pvIn, unsigned int iLenIn, void*& pvOut, unsigned int& iLenOut, Compressor comp=COMP_GZ);
 
 #endif
