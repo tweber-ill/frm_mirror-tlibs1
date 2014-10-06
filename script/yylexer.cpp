215ea8d9ed09cb5a0e0511d87e7addacb6bf87d9
Tobias Weber <tobias.weber@frm2.tum.de>
2014-10-06 09:14:47 +0200

    cosmetic change

--- script/yylexer.cpp
+++ script/yylexer.cpp
@@ -36,28 +36,24 @@ extern "C" int yylex(void* _yylval, void* _pParseObj)
 		return 1;
 	else if(tok.type == LEX_TOKEN_DOUBLE)
 	{
-		Node *pNode = new NodeDouble(tok.dVal);
-		yylval.pNode = pNode;
+		yylval.pNode = new NodeDouble(tok.dVal);
 		return TOK_DOUBLE;
 	}
 	else if(tok.type == LEX_TOKEN_INT)
 	{
-		Node *pNode = new NodeInt(tok.iVal);
-		yylval.pNode = pNode;
+		yylval.pNode = new NodeInt(tok.iVal);
 		return TOK_INT;
 	}
 	else if(tok.type == LEX_TOKEN_STRING)
 	{
-		Node *pNode = new NodeString(tok.strVal);
-		yylval.pNode = pNode;
+		yylval.pNode = new NodeString(tok.strVal);
 		return TOK_STRING;
 	}
 	else if(tok.type == LEX_TOKEN_CHAROP)
 		return tok.cOp;
 	else if(tok.type == LEX_TOKEN_IDENT)
 	{
-		Node *pNode = new NodeIdent(tok.strVal);
-		yylval.pNode = pNode;
+		yylval.pNode = new NodeIdent(tok.strVal);
 		return TOK_IDENT;
 	}
 	else
@@ -65,29 +61,30 @@ extern "C" int yylex(void* _yylval, void* _pParseObj)
 		switch(tok.type)
 		{
 			case LEX_TOKEN_IF: 		return TOK_IF;
-			case LEX_TOKEN_ELSE:	return TOK_ELSE;
+			case LEX_TOKEN_ELSE:		return TOK_ELSE;
 			case LEX_TOKEN_FOR:		return TOK_FOR;
-			case LEX_TOKEN_WHILE:	return TOK_WHILE;
-			case LEX_TOKEN_RETURN:	return TOK_RETURN;
-			case LEX_TOKEN_BREAK:	return TOK_BREAK;
+			case LEX_TOKEN_WHILE:		return TOK_WHILE;
+			case LEX_TOKEN_RETURN:		return TOK_RETURN;
+			case LEX_TOKEN_BREAK:		return TOK_BREAK;
 			case LEX_TOKEN_CONTINUE:	return TOK_CONTINUE;
-			case LEX_TOKEN_LOG_AND:	return TOK_LOG_AND;
-			case LEX_TOKEN_LOG_OR:	return TOK_LOG_OR;
-			case LEX_TOKEN_LOG_NOT:	return TOK_LOG_NOT;
-			case LEX_TOKEN_LOG_EQ:	return TOK_LOG_EQ;
-			case LEX_TOKEN_LOG_NEQ:	return TOK_LOG_NEQ;
+			case LEX_TOKEN_LOG_AND:		return TOK_LOG_AND;
+			case LEX_TOKEN_LOG_OR:		return TOK_LOG_OR;
+			case LEX_TOKEN_LOG_NOT:		return TOK_LOG_NOT;
+			case LEX_TOKEN_LOG_EQ:		return TOK_LOG_EQ;
+			case LEX_TOKEN_LOG_NEQ:		return TOK_LOG_NEQ;
 			case LEX_TOKEN_LOG_LESS:	return TOK_LOG_LESS;
 			case LEX_TOKEN_LOG_GREATER:	return TOK_LOG_GREATER;
-			case LEX_TOKEN_LOG_LEQ:	return TOK_LOG_LEQ;
-			case LEX_TOKEN_LOG_GEQ:	return TOK_LOG_GEQ;
-			case LEX_TOKEN_GLOBAL:	return TOK_GLOBAL;
+			case LEX_TOKEN_LOG_LEQ:		return TOK_LOG_LEQ;
+			case LEX_TOKEN_LOG_GEQ:		return TOK_LOG_GEQ;
+			case LEX_TOKEN_GLOBAL:		return TOK_GLOBAL;
 			default: break;
 		}
 	}
 
-	log_err("Line ", pParseObj->iCurLine,
+	log_err("line ", pParseObj->iCurLine,
 		" in \"", pParseObj->strCurFile,
-		"\"): Invalid token: ", tok.type);
+		"\": Invalid token: ", tok.type, 
+		".");
 	return 0;
 }
 
