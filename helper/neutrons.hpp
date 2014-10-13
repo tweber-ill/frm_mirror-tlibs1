27e92252fe0d080949fea0c0bb8e8b2274b9a4d8
Tobias Weber <tobias.weber@frm2.tum.de>
2014-10-13 09:03:25 +0200

    bugfixes

--- helper/neutrons.hpp
+++ helper/neutrons.hpp
@@ -316,12 +316,20 @@ template<class Sys, class Y>
 units::quantity<units::unit<units::plane_angle_dimension, Sys>, Y>
 get_angle_ki_Q(const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& ki,
 		const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& kf,
-		const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& Q)
+		const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& Q,
+		bool bPosSense=1)
 {
-	if(Q*(1e-10 * units::si::meter) == 0.)
-		return M_PI/2. * units::si::radians;
+	units::quantity<units::unit<units::plane_angle_dimension, Sys>, Y> angle;
 
-	return units::acos((ki*ki - kf*kf + Q*Q)/(2.*ki*Q));
+	if(Q*(1e-10 * units::si::meter) == 0.)
+		angle = M_PI/2. * units::si::radians;
+	else
+		angle = units::acos((ki*ki - kf*kf + Q*Q)/(2.*ki*Q));
+
+	if(!bPosSense)
+		angle = -angle;
+
+	return angle;
 }
 
 // Q_vec = ki_vec - kf_vec
@@ -332,12 +340,20 @@ template<class Sys, class Y>
 units::quantity<units::unit<units::plane_angle_dimension, Sys>, Y>
 get_angle_kf_Q(const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& ki,
 		const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& kf,
-		const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& Q)
+		const units::quantity<units::unit<units::wavenumber_dimension, Sys>, Y>& Q,
+		bool bPosSense=1)
 {
-	if(Q*(1e-10 * units::si::meter) == 0.)
-		return M_PI/2. * units::si::radians;
+	units::quantity<units::unit<units::plane_angle_dimension, Sys>, Y> angle;
 
-	return units::acos((-kf*kf + ki*ki - Q*Q)/(2.*kf*Q));
+	if(Q*(1e-10 * units::si::meter) == 0.)
+		angle = M_PI/2. * units::si::radians;
+	else
+		angle = units::acos((-kf*kf + ki*ki - Q*Q)/(2.*kf*Q));
+
+	if(!bPosSense)
+		angle = -angle;
+
+	return angle;
 }
 
 template<class Sys, class Y>
