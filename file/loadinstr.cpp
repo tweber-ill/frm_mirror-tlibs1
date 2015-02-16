f105b9e95eb73beadee4a130d44666b98578b43f
Tobias Weber <tobias.weber@frm2.tum.de>
2015-02-16 12:34:00 +0100

    continued with instrument-specific loaders

--- file/loadinstr.cpp
+++ file/loadinstr.cpp
@@ -46,6 +46,27 @@ void FilePsi::ReadData(std::istream& istr)
 	}
 }
 
+void FilePsi::GetInternalParams(const std::string& strAll, FilePsi::t_mapIParams& mapPara)
+{
+	std::vector<std::string> vecToks;
+	tl::get_tokens<std::string, std::string>(strAll, ",\n", vecToks);
+	//std::cout << strAll << std::endl;
+
+	for(const std::string& strTok : vecToks)
+	{
+		std::pair<std::string, std::string> pair =
+				tl::split_first<std::string>(strTok, "=", 1);
+
+		if(pair.first == "")
+			continue;
+
+		double dVal = tl::str_to_var<double>(pair.second);
+		mapPara.insert(t_mapIParams::value_type(pair.first, dVal));
+
+		//std::cout << "Key: " << pair.first << ", Val: " << dVal << std::endl;
+	}
+}
+
 bool FilePsi::Load(const char* pcFile)
 {
 	std::ifstream ifstr(pcFile);
@@ -76,6 +97,18 @@ bool FilePsi::Load(const char* pcFile)
 		}
 	}
 
+	t_mapParams::const_iterator iterParams = m_mapParams.find("PARAM"),
+				iterZeros = m_mapParams.find("ZEROS"),
+				iterVars = m_mapParams.find("VARIA"),
+				iterPos = m_mapParams.find("POSQE"),
+				iterSteps = m_mapParams.find("STEPS");
+
+	if(iterParams!=m_mapParams.end()) GetInternalParams(iterParams->second, m_mapParameters);
+	if(iterZeros!=m_mapParams.end()) GetInternalParams(iterZeros->second, m_mapParameters);
+	if(iterVars!=m_mapParams.end()) GetInternalParams(iterVars->second, m_mapParameters);
+	if(iterPos!=m_mapParams.end()) GetInternalParams(iterPos->second, m_mapParameters);
+	if(iterSteps!=m_mapParams.end()) GetInternalParams(iterSteps->second, m_mapParameters);
+
 	return true;
 }
 
@@ -101,6 +134,129 @@ void FilePsi::PrintParams(std::ostream& ostr) const
 	}
 }
 
+
+std::array<double,3> FilePsi::GetSampleLattice() const
+{
+	t_mapIParams::const_iterator iterA = m_mapParameters.find("AS");
+	t_mapIParams::const_iterator iterB = m_mapParameters.find("BS");
+	t_mapIParams::const_iterator iterC = m_mapParameters.find("CS");
+
+	double a = (iterA!=m_mapParameters.end() ? iterA->second : 0.);
+	double b = (iterB!=m_mapParameters.end() ? iterB->second : 0.);
+	double c = (iterC!=m_mapParameters.end() ? iterC->second : 0.);
+
+	return std::array<double,3> {a,b,c};
+}
+
+std::array<double,3> FilePsi::GetSampleAngles() const
+{
+	t_mapIParams::const_iterator iterA = m_mapParameters.find("AA");
+	t_mapIParams::const_iterator iterB = m_mapParameters.find("BB");
+	t_mapIParams::const_iterator iterC = m_mapParameters.find("CC");
+
+	double alpha = (iterA!=m_mapParameters.end() ? iterA->second/180.*M_PI : M_PI/2.);
+	double beta = (iterB!=m_mapParameters.end() ? iterB->second/180.*M_PI : M_PI/2.);
+	double gamma = (iterC!=m_mapParameters.end() ? iterC->second/180.*M_PI : M_PI/2.);
+
+	return std::array<double,3> {alpha, beta, gamma};
+}
+
+std::array<double,2> FilePsi::GetMonoAnaD() const
+{
+	t_mapIParams::const_iterator iterM = m_mapParameters.find("DM");
+	t_mapIParams::const_iterator iterA = m_mapParameters.find("DA");
+
+	double m = (iterM!=m_mapParameters.end() ? iterM->second : 3.355);
+	double a = (iterA!=m_mapParameters.end() ? iterA->second : 3.355);
+
+	return std::array<double,2> {m, a};
+}
+
+std::array<bool, 3> FilePsi::GetScatterSenses() const
+{
+	t_mapIParams::const_iterator iterM = m_mapParameters.find("SM");
+	t_mapIParams::const_iterator iterS = m_mapParameters.find("SS");
+	t_mapIParams::const_iterator iterA = m_mapParameters.find("SA");
+
+	bool m = (iterM!=m_mapParameters.end() ? iterM->second>0 : 0);
+	bool s = (iterM!=m_mapParameters.end() ? iterS->second>0 : 1);
+	bool a = (iterM!=m_mapParameters.end() ? iterA->second>0 : 0);
+
+	return std::array<bool,3> {m, s, a};
+}
+
+std::array<double, 3> FilePsi::GetScatterPlane0() const
+{
+	t_mapIParams::const_iterator iterX = m_mapParameters.find("AX");
+	t_mapIParams::const_iterator iterY = m_mapParameters.find("AY");
+	t_mapIParams::const_iterator iterZ = m_mapParameters.find("AZ");
+
+	double x = (iterX!=m_mapParameters.end() ? iterX->second : 1.);
+	double y = (iterY!=m_mapParameters.end() ? iterY->second : 0.);
+	double z = (iterZ!=m_mapParameters.end() ? iterZ->second : 0.);
+
+	return std::array<double,3> {x,y,z};
+}
+
+std::array<double, 3> FilePsi::GetScatterPlane1() const
+{
+	t_mapIParams::const_iterator iterX = m_mapParameters.find("BX");
+	t_mapIParams::const_iterator iterY = m_mapParameters.find("BY");
+	t_mapIParams::const_iterator iterZ = m_mapParameters.find("BZ");
+
+	double x = (iterX!=m_mapParameters.end() ? iterX->second : 0.);
+	double y = (iterY!=m_mapParameters.end() ? iterY->second : 1.);
+	double z = (iterZ!=m_mapParameters.end() ? iterZ->second : 0.);
+
+	return std::array<double,3> {x,y,z};
+}
+
+double FilePsi::GetKFix() const
+{
+	t_mapIParams::const_iterator iterK = m_mapParameters.find("KFIX");
+	double k = (iterK!=m_mapParameters.end() ? iterK->second : 0.);
+
+	return k;
+}
+
+std::array<double, 4> FilePsi::GetPosHKLE() const
+{
+	t_mapIParams::const_iterator iterH = m_mapPosHkl.find("QH");
+	t_mapIParams::const_iterator iterK = m_mapPosHkl.find("QK");
+	t_mapIParams::const_iterator iterL = m_mapPosHkl.find("QL");
+	t_mapIParams::const_iterator iterE = m_mapPosHkl.find("EN");
+
+	double h = (iterH!=m_mapPosHkl.end() ? iterH->second : 0.);
+	double k = (iterK!=m_mapPosHkl.end() ? iterK->second : 0.);
+	double l = (iterL!=m_mapPosHkl.end() ? iterL->second : 0.);
+	double E = (iterE!=m_mapPosHkl.end() ? iterE->second : 0.);
+
+	return std::array<double,4> {h,k,l,E};
+}
+
+std::array<double, 4> FilePsi::GetStepsHKLE() const
+{
+        t_mapIParams::const_iterator iterH = m_mapScanSteps.find("DQH");
+	if(iterH==m_mapScanSteps.end()) iterH = m_mapScanSteps.find("QH");
+
+        t_mapIParams::const_iterator iterK = m_mapScanSteps.find("DQK");
+	if(iterK==m_mapScanSteps.end()) iterK = m_mapScanSteps.find("QK");
+
+        t_mapIParams::const_iterator iterL = m_mapScanSteps.find("DQL");
+	if(iterL==m_mapScanSteps.end()) iterL = m_mapScanSteps.find("QL");
+
+        t_mapIParams::const_iterator iterE = m_mapScanSteps.find("DEN");
+	if(iterE==m_mapScanSteps.end()) iterE = m_mapScanSteps.find("EN");
+
+
+        double h = (iterH!=m_mapScanSteps.end() ? iterH->second : 0.);
+        double k = (iterK!=m_mapScanSteps.end() ? iterK->second : 0.);
+        double l = (iterL!=m_mapScanSteps.end() ? iterL->second : 0.);
+        double E = (iterE!=m_mapScanSteps.end() ? iterE->second : 0.);
+
+        return std::array<double,4> {h,k,l,E};
+}
+
 }
 
 
@@ -108,15 +264,20 @@ void FilePsi::PrintParams(std::ostream& ostr) const
 
 /*
 // test
-// gcc -o 0 file/loadinstr.cpp helper/log.cpp string/string.cpp -std=c++11 -lstdc++
+// gcc -o 0 file/loadinstr.cpp helper/log.cpp -std=c++11 -lstdc++
 int main()
 {
 	tl::FilePsi psi;
-	if(!psi.Load("/home/tw/Measurements/Eiger2014/eiger2014n001051.scn"))
+	if(!psi.Load("/home/tweber/tmp/tst.scn"))
 	{
 		tl::log_err("Cannot load data file.");
 		return -1;
 	}
+
+	std::cout << "Lattice: ";
+	for(int i=0; i<3; ++i)
+		std::cout << psi.GetSampleLattice()[i] << ", ";
+	std::cout << std::endl;
 
 	psi.PrintParams(std::cout);
 
--- file/loadinstr.h
+++ file/loadinstr.h
@@ -10,17 +10,23 @@
 
 #include <unordered_map>
 #include <vector>
+#include <array>
 #include <iostream>
 #include "../string/string.h"
 
 namespace tl{
 
-// psi files
+// psi & ill files
 class FilePsi
 {
 	public:
 		typedef std::unordered_map<std::string, std::string> t_mapParams;
 		t_mapParams m_mapParams;
+
+		// internal parameters in m_mapParams
+		typedef std::unordered_map<std::string, double> t_mapIParams;
+		t_mapIParams m_mapParameters, m_mapZeros, m_mapVariables, m_mapPosHkl, m_mapScanSteps;
+
 
 		typedef std::vector<std::string> t_vecColNames;
 		t_vecColNames m_vecColNames;
@@ -32,18 +38,35 @@ class FilePsi
 
 	protected:
 		void ReadData(std::istream& istr);
+		void GetInternalParams(const std::string& strAll, t_mapIParams& mapPara);
 
 	public:
 		FilePsi() = default;
 		virtual ~FilePsi() = default;
 
 		bool Load(const char* pcFile);
+
 		void PrintParams(std::ostream& ostr) const;
+		const t_mapParams& GetParams() const { return m_mapParams; }
 
 		const std::string& GetColName(std::size_t iCol) const { return m_vecColNames[iCol]; }
 		std::size_t GetColCount() const { return m_vecColNames.size(); }
 		const std::vector<double>& GetCol(std::size_t iCol) const { return m_vecData[iCol]; }
 		const std::vector<double>& GetCol(const std::string& strName) const;
+
+	public:
+		std::array<double, 3> GetSampleLattice() const;
+		std::array<double, 3> GetSampleAngles() const;
+		std::array<double, 2> GetMonoAnaD() const;
+
+		std::array<bool, 3> GetScatterSenses() const;
+		std::array<double, 3> GetScatterPlane0() const;
+		std::array<double, 3> GetScatterPlane1() const;
+
+		double GetKFix() const;
+
+		std::array<double, 4> GetPosHKLE() const;	// zero pos.
+		std::array<double, 4> GetStepsHKLE() const;	// scan steps
 };
 
 }
