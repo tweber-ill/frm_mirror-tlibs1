82fa32542d84db672d3d515e549c12ae9f16c415
Tobias Weber <tobias.weber@frm2.tum.de>
2015-07-09 09:58:10 +0200

    continued with dispersion stuff

--- math/disp.h
+++ math/disp.h
@@ -15,12 +15,116 @@
 
 namespace tl {
 
+
+// ----------------------------------------------------------------------------
+
+// atom positions
+enum class UCType
+{
+	SIMPLE,
+	FCC,
+	BCC,
+};
+
+// iDist == 0: nearest neighbours
+// iDist == 1: next-nearest neighbours
+template<typename T=double>
+std::vector<ublas::vector<T>> get_neighbour_atoms(UCType crys, int iDist=0, T a=1.)
+{
+	std::vector<ublas::vector<T>> vecAtoms;
+
+	if(crys == UCType::SIMPLE)
+	{
+		if(iDist == 0)
+		{
+			vecAtoms.push_back(make_vec({1., 0., 0.}));
+			vecAtoms.push_back(make_vec({0., 1., 0.}));
+			vecAtoms.push_back(make_vec({0., 0., 1.}));
+			vecAtoms.push_back(make_vec({-1., 0., 0.}));
+			vecAtoms.push_back(make_vec({0., -1., 0.}));
+			vecAtoms.push_back(make_vec({0., 0., -1.}));
+		}
+		else if(iDist == 1)
+		{
+			vecAtoms.push_back(make_vec({1., 1., 0.}));
+			vecAtoms.push_back(make_vec({1., -1., 0.}));
+			vecAtoms.push_back(make_vec({-1., 1., 0.}));
+			vecAtoms.push_back(make_vec({-1., -1., 0.}));
+			vecAtoms.push_back(make_vec({1., 0., 1.}));
+			vecAtoms.push_back(make_vec({1., 0., -1.}));
+			vecAtoms.push_back(make_vec({-1., 0., 1.}));
+			vecAtoms.push_back(make_vec({-1., 0., -1.}));
+			vecAtoms.push_back(make_vec({0., 1., 1.}));
+			vecAtoms.push_back(make_vec({0., 1., -1.}));
+			vecAtoms.push_back(make_vec({0., -1., 1.}));
+			vecAtoms.push_back(make_vec({0., -1., -1.}));
+		}
+	}
+	else if(crys == UCType::FCC)
+	{
+		if(iDist == 0)
+		{
+			vecAtoms.push_back(make_vec({0.5, 0.5, 0.}));
+			vecAtoms.push_back(make_vec({0.5, -0.5, 0.}));
+			vecAtoms.push_back(make_vec({-0.5, 0.5, 0.}));
+			vecAtoms.push_back(make_vec({-0.5, -0.5, 0.}));
+			vecAtoms.push_back(make_vec({0.5, 0., 0.5}));
+			vecAtoms.push_back(make_vec({0.5, 0., -0.5}));
+			vecAtoms.push_back(make_vec({-0.5, 0., 0.5}));
+			vecAtoms.push_back(make_vec({-0.5, 0., -0.5}));
+			vecAtoms.push_back(make_vec({0., 0.5, 0.5}));
+			vecAtoms.push_back(make_vec({0., 0.5, -0.5}));
+			vecAtoms.push_back(make_vec({0., -0.5, 0.5}));
+			vecAtoms.push_back(make_vec({0., -0.5, -0.5}));
+		}
+		else if(iDist == 1)
+		{
+			vecAtoms.push_back(make_vec({1., 0., 0.}));
+			vecAtoms.push_back(make_vec({0., 1., 0.}));
+			vecAtoms.push_back(make_vec({0., 0., 1.}));
+			vecAtoms.push_back(make_vec({-1., 0., 0.}));
+			vecAtoms.push_back(make_vec({0., -1., 0.}));
+			vecAtoms.push_back(make_vec({0., 0., -1.}));
+		}
+	}
+	else if(crys == UCType::BCC)
+	{
+		if(iDist == 0)
+		{
+			vecAtoms.push_back(make_vec({0.5, 0.5, 0.5}));
+			vecAtoms.push_back(make_vec({0.5, 0.5, -0.5}));
+			vecAtoms.push_back(make_vec({0.5, -0.5, 0.5}));
+			vecAtoms.push_back(make_vec({0.5, -0.5, -0.5}));
+			vecAtoms.push_back(make_vec({-0.5, 0.5, 0.5}));
+			vecAtoms.push_back(make_vec({-0.5, 0.5, -0.5}));
+			vecAtoms.push_back(make_vec({-0.5, -0.5, 0.5}));
+			vecAtoms.push_back(make_vec({-0.5, -0.5, -0.5}));
+		}
+		else if(iDist == 1)
+		{
+			vecAtoms.push_back(make_vec({1., 0., 0.}));
+			vecAtoms.push_back(make_vec({0., 1., 0.}));
+			vecAtoms.push_back(make_vec({0., 0., 1.}));
+			vecAtoms.push_back(make_vec({-1., 0., 0.}));
+			vecAtoms.push_back(make_vec({0., -1., 0.}));
+			vecAtoms.push_back(make_vec({0., 0., -1.}));
+		}
+	}
+
+	if(a != 1.)
+	for(ublas::vector<T>& vec : vecAtoms)
+		vec *= a;
+
+	return vecAtoms;
+}
+
+
+
 // ----------------------------------------------------------------------------
 
 // structure factor
-template<typename T=double>
-std::complex<T> structfact(const std::initializer_list<ublas::vector<T>>& lstAtoms, 
-	const ublas::vector<T>& vecG)
+template<typename T=double, typename t_cont=std::initializer_list<ublas::vector<T>>>
+std::complex<T> structfact(const t_cont& lstAtoms, const typename t_cont::value_type& vecG)
 {
 	constexpr std::complex<T> i(0., 1.);
 	std::complex<T> F(0., 0.);
@@ -35,16 +139,15 @@ std::complex<T> structfact(const std::initializer_list<ublas::vector<T>>& lstAto
 // ----------------------------------------------------------------------------
 
 // coupling J and atom position
-template<typename T=double> using t_atompos = std::pair<T, ublas::vector<T>>;
+template<typename T=double> using t_magatompos = std::pair<T, ublas::vector<T>>;
 
-template<typename T=double>
-T ferromag(const std::initializer_list<t_atompos<T>>& lstAtoms, 
-	const ublas::vector<T>& vecq, T tS)
+template<typename T=double, typename t_cont=std::initializer_list<t_magatompos<T>>>
+T ferromag(const t_cont& lstAtoms, const ublas::vector<T>& vecq, T tS)
 {
 	std::complex<T> J(0., 0.), J0(0., 0.);
 	constexpr std::complex<T> i(0., 1.);
 
-	for(const t_atompos<T>& atom : lstAtoms)
+	for(const t_magatompos<T>& atom : lstAtoms)
 	{
 		const T& tJ = atom.first;
 		const ublas::vector<T>& vecPos = atom.second;
--- test/ferro.cpp
+++ test/ferro.cpp
@@ -1,33 +1,91 @@
-// clang -o ferro test/ferro.cpp -lstdc++ -lm -std=c++11
+// clang -o ferro test/ferro.cpp gfx/gnuplot.cpp helper/log.cpp  -lstdc++ -lm -lboost_iostreams -std=c++11
 
 #include "../math/disp.h"
+#include "../math/neutrons.hpp"
 #include "../gfx/gnuplot.h"
 #include <iostream>
 
+using namespace tl;
+
 int main()
 {
-	std::vector<double> vecq = tl::linspace(0., 5., 128);
-	std::vector<double> vecE;
-	vecE.reserve(vecq.size());
+	double k = co::k_B / one_meV * kelvin;
+	double a = 5.;
+	double J1 = 0.5*k;
+	double J2 = 0.1*k;
+	double S = 7./2.;
+
+	std::vector<ublas::vector<double>> vecAtomsN0 = get_neighbour_atoms(UCType::FCC, 0, a);
+	std::vector<ublas::vector<double>> vecAtomsN1 = get_neighbour_atoms(UCType::FCC, 1, a);
+
+	std::vector<t_magatompos<double>> vecMagAtoms;
+	for(const ublas::vector<double>& vec : vecAtomsN0)
+		vecMagAtoms.push_back(t_magatompos<double>(J1, vec));
+	for(const ublas::vector<double>& vec : vecAtomsN1)
+		vecMagAtoms.push_back(t_magatompos<double>(J2, vec));
+
+
+
+	std::vector<double> vecq = linspace(0., 1.5, 128);
+	std::vector<double> vecE1, vecE11, vecE111;
+	vecE1.reserve(vecq.size());
+	vecE11.reserve(vecq.size());
+	vecE111.reserve(vecq.size());
 
 	for(double q : vecq)
 	{
-		double E = tl::ferromag({
-			{1., tl::make_vec({1., 0., 0.})},
-			{1., tl::make_vec({-1., 0., 0.})},
-			{1., tl::make_vec({0., 1., 0.})},
-			{1., tl::make_vec({0., -1., 0.})},
-			{1., tl::make_vec({0., 0., 1.})},
-			{1., tl::make_vec({0., 0., -1.})},
-				},
-			tl::make_vec({q, 0., 0.}), 1.);
+		ublas::vector<double> vec1 = make_vec({1., 0., 0.});
+		ublas::vector<double> vec11 = make_vec({1., 1., 0.}); vec11 /= ublas::norm_2(vec11);
+		ublas::vector<double> vec111 = make_vec({1., 1., 1.}); vec111 /= ublas::norm_2(vec111);
 
-		vecE.push_back(E);
-		//std::cout << q << " " << E << std::endl;
+		vec1 *= q; vec11 *= q; vec111 *= q;
+
+		double E1 = ferromag(vecMagAtoms, vec1, S);
+		double E11 = ferromag(vecMagAtoms, vec11, S);
+		double E111 = ferromag(vecMagAtoms, vec111, S);
+
+		vecE1.push_back(E1);
+		vecE11.push_back(E11);
+		vecE111.push_back(E111);
 	}
 
-	tl::GnuPlot plt;
+
+	GnuPlot plt;
 	plt.Init();
-	plt.SimplePlot(vecq, vecE, std::vector<double>(), std::vector<double>(), tl::STYLE_LINES_SOLID);
+	plt.SetLegendPlace("bottom right");
+	plt.SetXLabel("q (1/A)");
+	plt.SetYLabel("E (meV)");
+	plt.StartPlot();
+
+	PlotObj obj1;
+	obj1.linestyle = STYLE_LINES_SOLID;
+	obj1.vecX = vecq;
+	obj1.vecY = vecE1;
+	obj1.bHasColor = 1;
+	obj1.iColor = 0xff0000;
+	obj1.strLegend = "[100]";
+	plt.AddLine(obj1);
+
+	PlotObj obj11;
+	obj11.linestyle = STYLE_LINES_SOLID;
+	obj11.vecX = vecq;
+	obj11.vecY = vecE11;
+	obj11.bHasColor = 1;
+	obj11.iColor = 0x009900;
+	obj11.strLegend = "[110]";
+	plt.AddLine(obj11);
+
+	PlotObj obj111;
+	obj111.linestyle = STYLE_LINES_SOLID;
+	obj111.vecX = vecq;
+	obj111.vecY = vecE111;
+	obj111.bHasColor = 1;
+	obj111.iColor = 0x0000ff;
+	obj111.strLegend = "[111]";
+	plt.AddLine(obj111);
+
+	plt.FinishPlot();
+
+	//plt.SimplePlot(vecq, vecE111, std::vector<double>(), std::vector<double>(), STYLE_LINES_SOLID);
 	return 0;
 }
