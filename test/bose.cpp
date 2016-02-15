f270b2f8f93cd5aac85e4338717991ddf6c17d6e
Tobias Weber <tobias.weber@frm2.tum.de>
2016-02-15 13:54:44 +0100

    bose test

---
+++ test/bose.cpp
@@ -0,0 +1,17 @@
+// gcc -I. -o bose test/bose.cpp -std=c++11 -lstdc++ -lm
+
+#include <iostream>
+#include <fstream>
+#include "math/neutrons.hpp"
+
+int main()
+{
+	std::ofstream ofstr("bosetst.dat");
+	double dT = 80.;
+
+	for(double dE=1.; dE<10.; dE+=0.1)
+	{
+		double dBose = tl::bose<double>(dE, dT);
+		ofstr << dE << "\t" << dBose << "\n";
+	}
+}
