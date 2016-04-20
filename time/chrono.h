a6efa687cd156760db1e3dbe30c7c2abfa3c3394
Tobias Weber <tobias.weber@frm2.tum.de>
2016-04-20 12:29:36 +0200

    changes in time lib

--- time/chrono.h
+++ time/chrono.h
@@ -13,18 +13,34 @@
 
 namespace tl {
 
+// like std::chrono::seconds/minutes/hours, but with variable type
+template<typename T = long >
+using t_dur_secs = std::chrono::duration<T, std::ratio<1, 1>>;
+template<typename T = long >
+using t_dur_mins = std::chrono::duration<T, std::ratio<60, 1>>;
+template<typename T = long >
+using t_dur_hours = std::chrono::duration<T, std::ratio<60*60, 1>>;
+
+template<typename T = long >
+using t_dur_days = std::chrono::duration<T, std::ratio<60*60*24, 1>>;
+
+template<typename T = long >
+using t_dur_weeks = std::chrono::duration<T, std::ratio<60*60*24*7, 1>>;
+
+
+// duration since epoch
+template<typename t_dur = std::chrono::seconds>
+t_dur epoch_dur()
+{
+	namespace ch = std::chrono;
+	return ch::duration_cast<t_dur>(ch::system_clock::now().time_since_epoch());
+}
+
+// seconds since epoch
 template<typename T=double>
 T epoch()
 {
-	namespace ch = std::chrono;
-
-	using t_dur = ch::system_clock::duration;
-	using t_per = t_dur::period;
-
-	const T tSecsPerPeriod = T(t_per::num)/T(t_per::den);
-	const t_dur durNow = ch::system_clock::now().time_since_epoch();
-
-	return tSecsPerPeriod * T(durNow.count());
+	return epoch_dur<t_dur_secs<T>>().count();
 }
 
 }
