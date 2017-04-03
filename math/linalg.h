e6803322cc865c255f4e759293d2358ada4d4e7e
Tobias Weber <tobias.weber@frm2.tum.de>
2017-04-03 08:26:06 +0200

    comment

--- math/linalg.h
+++ math/linalg.h
@@ -767,12 +767,14 @@ bool inverse(const mat_type& mat, mat_type& inv)
 
 /**
  * R = T^(-1) M T
+ * bCongr==1: do a congruence trafo
+ * bCongr==0: do a similarity trafo
  */
 template<class mat_type=ublas::matrix<double>>
-mat_type transform(const mat_type& mat, const mat_type& matTrafo, bool bOrtho=0)
+mat_type transform(const mat_type& mat, const mat_type& matTrafo, bool bCongr=0)
 {
 	mat_type matTrafoInv;
-	if(bOrtho)
+	if(bCongr)
 		matTrafoInv = ublas::trans(matTrafo);
 	else
 		inverse(matTrafo, matTrafoInv);
@@ -785,12 +787,14 @@ mat_type transform(const mat_type& mat, const mat_type& matTrafo, bool bOrtho=0)
 
 /**
  * R = T M T^(-1)
+ * bCongr==1: do a congruence trafo
+ * bCongr==0: do a similarity trafo
  */
 template<class mat_type=ublas::matrix<double>>
-mat_type transform_inv(const mat_type& mat, const mat_type& matTrafo, bool bOrtho=0)
+mat_type transform_inv(const mat_type& mat, const mat_type& matTrafo, bool bCongr=0)
 {
 	mat_type matTrafoInv;
-	if(bOrtho)
+	if(bCongr)
 		matTrafoInv = ublas::trans(matTrafo);
 	else
 		inverse(matTrafo, matTrafoInv);
