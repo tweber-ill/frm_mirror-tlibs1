07b0728cba579aaeac8d8589377e93a6435054a8
Tobias Weber <tobias.weber@frm2.tum.de>
2017-06-17 09:33:03 +0200

    jl plotting test

--- ext/tlibs.jl
+++ ext/tlibs.jl
@@ -20,6 +20,7 @@ function __init__()
 end
 
 
+
 #
 # loads instrument data files
 #
@@ -103,16 +104,36 @@ function fit(fkt, x, y, yerr; fixed = [], values = Dict(), errors = Dict())
 	end
 
 	dictRet["<valid>"] = bOk
+	dictRet["<func>"] = fkt
+	dictRet["<args>"] = arrParams
+	dictRet["<args_val>"] = arrValues
+	dictRet["<args_err>"] = arrErrors
 	return dictRet
 end
+
+
+
+#
+# evaluate the fitted function using the fitted parameters
+#
+function eval_func(fitresult, xmin, xmax, count=128)
+	fkt = fitresult["<func>"]
+
+	xs = linspace(xmin, xmax, count)
+	ys = fkt(xs, fitresult["<args_val>"]...)
+
+	return [ Array(xs), Array(ys) ]
+end
+
 
 
 #
 # Gauss model
 #
 function gauss_model_amp(x, x0, sigma, amp, offs)
-	return amp * exp(-0.5 * ((x-x0)/sigma)*((x-x0)/sigma)) + offs
+	return amp * exp(-0.5 * ((x-x0)/sigma).*((x-x0)/sigma)) + offs
 end
+
 
 #
 # Lorentz model
@@ -123,38 +144,4 @@ end
 
 
 
-end 	# tl
-
-
-
-
-# -----------------------------------------------------------------------------
-# test
-
-
-a = tl.loadinstr("/home/tweber/Measurements/mira-mgv2o4-17/data/11797_00025700.dat")
-cols = a[1]
-data = a[2]
-#println(cols)
-#println(typeof(data))
-
-idxE = findfirst(cols, "E")
-idxCtr = findfirst(cols, "ctr1")
-
-Es = data[:,idxE]
-cts = data[:,idxCtr]
-cts_err = sqrt(cts)
-
-println(lpad(cols[idxE], 12), " ", lpad(cols[idxCtr], 12))
-for (E, ct) in zip(Es, cts)
-	println(lpad(E, 12), " ", lpad(ct, 12))
-end
-
-
-
-fitresult = tl.fit(tl.gauss_model_amp, Es, cts, cts_err, fixed = ["offs"],
-	values = Dict("x0" => -1.5, "sigma" => 0.5, "amp" => 100., "offs" => 50.),
-	errors = Dict("x0" => 0.25, "sigma" => 0.25, "amp" => 20., "offs" => 10.))
-println(fitresult)
-
-# -----------------------------------------------------------------------------
+end 	# module tl
